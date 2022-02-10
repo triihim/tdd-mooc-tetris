b5630d988ae8db828aac423f2fe29128a97c5f06
Tatu Riihimäki <tatu@riihimaki.com>
2022-02-10 18:04:37 +0200

    feat: dropped block starts from top middle

--- src/Board.mjs
+++ src/Board.mjs
@@ -1,13 +1,32 @@
 export class Board {
   width;
   height;
+  state;
 
   constructor(width, height) {
     this.width = width;
     this.height = height;
+    this.state = [];
+    for(let row = 0; row < width; row++) {
+      this.state[row] = [];
+      for(let col = 0; col < height; col++) {
+        this.state[row][col] = '.';
+      }
+    }
+  }
+
+  drop(block) {
+    this.state[0][1] = block.color;
   }
 
   toString() {
-    return '...\n...\n...\n'
+    let s = '';
+    for(let row = 0; row < this.width; row++) {
+      for(let col = 0; col < this.height; col++) {
+        s += this.state[row][col];
+      }
+      s += '\n';
+    }
+    return s;
   }
 }
