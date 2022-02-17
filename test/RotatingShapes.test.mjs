ea2e1f4f31bed68860b037980d6921a25da49d8e
Tatu Riihimäki <tatu.riihimaki@gmail.com>
2022-02-17 19:31:43 +0200

    feat: 5x5 shape can be rotated both left and right

--- test/RotatingShapes.test.mjs
+++ test/RotatingShapes.test.mjs
@@ -33,7 +33,7 @@ describe("Rotating 3x3 shape", () => {
   });
 });
 
-/*
+
 describe("Rotating 5x5 shape", () => {
   const shape = new RotatingShape(
     `ABCDE
@@ -53,7 +53,7 @@ describe("Rotating 5x5 shape", () => {
     );
   });
 
-  xit("can be rotated right/clockwise", () => {
+  it("can be rotated right/clockwise", () => {
     expect(shape.rotateRight().toString()).to.equalShape(
       `UPKFA
        VQLGB
@@ -63,7 +63,7 @@ describe("Rotating 5x5 shape", () => {
     );
   });
 
-  xit("can be rotated left/counter-clockwise", () => {
+  it("can be rotated left/counter-clockwise", () => {
     expect(shape.rotateLeft().toString()).to.equalShape(
       `EJOTY
        DINSX
@@ -73,4 +73,4 @@ describe("Rotating 5x5 shape", () => {
     );
   });
 });
-*/
+
