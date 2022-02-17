4705f9569cbe404fc52a94a9f56fc126179dd3b3
Tatu Riihimäki <tatu.riihimaki@gmail.com>
2022-02-17 19:17:11 +0200

    feat: 3x3 shape can be rotated right

--- src/RotatingShape.mjs
+++ src/RotatingShape.mjs
@@ -1,9 +1,21 @@
 export class RotatingShape {
     constructor(shape) {
-        this.shape = shape.split(' ').join('') + '\n';
+        const rows = shape.split('\n');
+        this.sideLength = rows.length;
+        this.shape = rows.reduce((shape, row) => shape.concat(row.trim()), []);
+    }
+
+    rotateRight() {
+        const newShape = [...Array(this.sideLength)].map(e => Array(this.sideLength));
+        for(let row = 0; row < this.sideLength; row++) {
+            for(let col = 0; col < this.sideLength; col++) {
+                newShape[row][col] = this.shape[this.sideLength - col - 1][row];
+            }
+        }
+        return new RotatingShape(newShape.map(row => row.join('')).join('\n'));
     }
 
     toString() {
-        return this.shape;
+        return this.shape.join('\n') + '\n';
     }
 }
